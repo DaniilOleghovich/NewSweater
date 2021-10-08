40e79d6b8498cb17e27c6e48793631f9d1cdeba7
DaniilOleghovich <danya.pavlichenko.1997@mail.ru>
2021-10-09 00:50:40 +0300

    Added addMessage and filterMessages methods and fixed autoadding of new element to DB after reloading of page(added redirect to the new page - messages)

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -1,18 +1,27 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Message;
+import com.example.demo.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
 
+    @Autowired
+    private MessageRepository messageRepository;
+
     @GetMapping
-    public String hello() {
-        return "hello";
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "main";
     }
 
     @GetMapping("/greeting")
@@ -20,4 +29,32 @@ public class GreetingController {
         model.put("name", name);
         return "greeting";
     }
+
+    @GetMapping("/messages")
+    public String getMessages(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "messages";
+    }
+
+    @PostMapping
+    public String addMessage(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepository.save(message);
+        return "redirect:/messages";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+             messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+
+        model.put("messages", messages);
+        return "main";
+    }
+
 }
--- src/main/java/com/example/demo/repository/MessageRepository.java
+++ src/main/java/com/example/demo/repository/MessageRepository.java
@@ -3,5 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
 }
